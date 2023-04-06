cb37d77d52d1de6f2c63ba71ceefdedc3677028a
Michael <msousacode@gmail.com>
2023-04-06 16:55:26 -0300

    feat: create jwt authentication token.

---
+++ src/main/java/com/msousa/cognito/security/jwt/JwtAuthenticationToken.java
@@ -0,0 +1,32 @@
+package com.msousa.cognito.security.jwt;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Object principal;
+    private final JWTClaimsSet jwtClaimsSet;
+
+    public JwtAuthenticationToken(Object principal, JWTClaimsSet jwtClaimsSet, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.jwtClaimsSet = jwtClaimsSet;
+        super.setAuthenticated(true);
+    }
+
+    public Object getCredentials() {
+        return null;
+    }
+
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
+    public JWTClaimsSet getJwtClaimsSet() {
+        return this.jwtClaimsSet;
+    }
+}
