40b2fe3a89b0ffee4490121d0f21a1def1ba6030
Michael <msousacode@gmail.com>
2023-04-06 15:57:00 -0300

    init: initial commit

---
+++ src/test/java/com/msousa/cognito/AwsCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msousa.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
